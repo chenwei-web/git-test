1795dccbfefea5c93fcdafc570d547bd79133c4d
CW <CW@qq.com>
2022-01-17 12:49:33 +0800

    gitee-test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git3!");
         System.out.println("push test");
+        System.out.println("push gitee-test");
+
     }
 }
